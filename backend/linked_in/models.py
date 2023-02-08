306297049a77a18d4a168798657a5c2ca1180820
Joseph Smith <jsmith47265@gmail.com>
2023-02-08 18:03:06 -0500

    Job model setup and migrations

--- backend/linked_in/models.py
+++ backend/linked_in/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Job(models.Model):
     title = models.CharField(max_length=50)
     company_name = models.CharField(max_length=50)
+    company_logo_url = models.CharField(max_length=100)
     job_type = models.CharField(max_length=20)
     remote_status = models.CharField(max_length=8)
     salary_start = models.IntegerField()
     salary_end = models.IntegerField()
-    date_listed = models.DateTimeField()
+    date_listed = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self) -> str:
+        return self.title
+    
